91e34ec334eb9f7c37a49f650790bdec6e78348c
Li Syuen <syuen@MacBook-Air-3.local>
2026-04-09 15:36:07 +0800

    Chore: add database seed file for team setup

---
+++ backend/src/main/resources/database_setup.sql
@@ -0,0 +1,11 @@
+USE expense_tracker;
+
+-- Insert Categories
+INSERT INTO category (category_name, enabled, transaction_type_id) VALUES 
+('Other', 1, 1),
+('Food', 1, 1),
+('Leisure', 1, 1),
+('Household', 1, 1),
+('Clothing', 1, 1),
+('Education', 1, 1),
+('Healthcare', 1, 1);
